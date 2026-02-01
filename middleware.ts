26ce2dbd0a9230b90663c1b17f70ec6ec06c31ce
Naveen Karthick <karthicknk97@gmail.com>
2026-02-01 13:14:45 +0530

    Added reminder functionality

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.png$|sw.js|workbox-.*.js).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.png$|sw.js|workbox-.*.js|api/cron).*)"],
 };
